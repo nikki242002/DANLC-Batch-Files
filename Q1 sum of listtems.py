8bc1c1f49ed685f7f8ff1c54b2295e4dd28bd5d7
nikki242002 <sharmanitin242002@gmail.com>
2025-01-13 21:29:00 +0530

    Update Q1 sum of listtems.py

--- Q1 sum of listtems.py
+++ Q1 sum of listtems.py
@@ -1,4 +1,4 @@
-#Q1 Write a Python program to sum all the items in a list.
+#Q1.Write a Python program to sum all the items in a list.
 numbers = [10, 20, 30, 40, 50]
 total_sum = 0
 for number in numbers:
