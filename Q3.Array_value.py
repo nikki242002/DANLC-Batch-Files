62201b82b675fe26c59d2fbb5e0f71c40b7938df
Nitin <sharmanitin242002@gmail.com>
2025-01-21 22:36:04 +0530

    Delete Q3.Array_value.py

--- Q3.Array_value.py
+++
@@ -1,6 +0,0 @@
-#Q3.Write a NumPy program to create an array with values ranging from 12 to 38.
-
-import numpy as np
-arr = np.arange(12,38)
-print("Array with values ranging from 12 to 38 are:")
-print(arr)
