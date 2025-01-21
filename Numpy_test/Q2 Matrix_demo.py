f2c9df576a13a8cabdec414253fb3ecb938b2738
Nitin <sharmanitin242002@gmail.com>
2025-01-21 21:11:27 +0530

    Add files via upload

---
+++ Numpy_test/Q2 Matrix_demo.py
@@ -0,0 +1,6 @@
+#Q2.Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.
+
+import numpy as np
+matrix = np.arange(2, 11).reshape(3, 3)
+print("3x3 matrix values ranging from 2 to 10 are:")
+print(matrix)
---
+++ Numpy_test/Q3.Array_value.py
@@ -0,0 +1,6 @@
+#Q3.Write a NumPy program to create an array with values ranging from 12 to 38.
+
+import numpy as np
+arr = np.arange(12,38)
+print("Array with values ranging from 12 to 38 are:")
+print(arr)
