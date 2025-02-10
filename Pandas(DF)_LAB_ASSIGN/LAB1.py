fe67ef693c6a6f4d939b2898c0549fd8ad3cdac5
Nitin Sharma <sharmanitin242002@gmail.com>
2025-02-10 20:54:11 +0530

    Add files via upload

---
+++ Pandas(DF)_LAB_ASSIGN/LAB1.py
@@ -0,0 +1,12 @@
+#LAB1.Write a Pandas program to create a dataframe from a dictionary and display it.
+ # Sample data:
+ # score={'Math':[78,85,96,80,86], 'English':[84,94,89,83,86],'Hindi':[86,97,96,72,83]}
+
+import pandas as pd
+score = {
+    'Math': [78, 85, 96, 80, 86],
+    'English': [84, 94, 89, 83, 86],
+    'Hindi': [86, 97, 96, 72, 83]
+}
+df = pd.DataFrame(score)
+print(df)
